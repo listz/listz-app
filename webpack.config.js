c732fce4089e2007596563038057dce3292683de
artus <artusvranken@gmail.com>
2018-10-31 18:39:50 +0100

    Had an issue with the UglifyJsPlugin, I disabled it for now.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/listz-app/dist/',
     filename: 'build.js'
   },
   module: {
@@ -66,12 +66,12 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
+    /*new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
         warnings: false
       }
-    }),
+    }),*/
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
