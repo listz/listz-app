144eaf925fb137e3bad2772e819b748d304b636f
artus <artusvranken@gmail.com>
2018-11-10 13:43:30 +0100

    Trying to publish to gh-pages.

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,13 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
+
+  module.exports.output = {
+    path: path.resolve(__dirname, './dist'),
+      publicPath: './listz-app/dist/',
+        filename: 'build.js'
+  }
+
   module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
